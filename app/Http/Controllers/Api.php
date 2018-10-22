cf27abd2bcc75abf78f9fc7543f7ab6e61d67a59
tianna <tianna@tianna.com>
2018-10-22 11:08:01 +0800

    初始化

---
+++ app/Http/Controllers/Api.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 郑庆添
+ * Date: 2018/10/21
+ * Time: 22:13
+ */
+
+namespace App\Http\Controllers;
+
+
+class Api extends Controller
+{
+    /**
+     * 上报尚未建立联系之时用户发送的信息
+     * @url ims/api/v1/formsave
+     * @method POST
+     * @param int $openid openid
+     * @param int $form_id formid
+     * @return int $code 状态码
+     * @return string $msg 描述信息
+     */
+    public function formsave(){
+            return [
+                'code' => -5,
+                'msg'  => "插入失败"
+            ];
+    }
+}
---
+++ database/migrations/2018_10_21_135447_create_table_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',32);
+            $table->string('password',64);
+            $table->string('email',64);
+            $table->unsignedSmallInteger('level')->default(1);
+            $table->dateTime('last_login')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
