c292c95b25262cb4058db89c5a0248425a576752
zqt6655 <125550581@qq.com>
2018-10-24 18:37:48 +0800

    搜索加分页

--- app/Relationship.php
+++ app/Relationship.php
@@ -119,16 +119,23 @@ class Relationship extends Model
     }
 
     public function search($keyword){
-        return $this::where('is_delete', '=', 0)
+        return DB::table("$this->table_name as r")
+            ->leftJoin('category as c','r.category_id','=','c.id')
+            ->where('r.is_delete', '=', 0)
+//            $this::where('is_delete', '=', 0)
             ->where(function($query) use ($keyword){
-                $query->where('name', 'like', '%'.$keyword.'%')
-                    ->orWhere('phone', 'like', '%'.$keyword.'%')
-                    ->orWhere('email', 'like', '%'.$keyword.'%')
-                    ->orWhere('company', 'like', '%'.$keyword.'%')
-                    ->orWhere('position', 'like', '%'.$keyword.'%')
-                    ->orWhere('title', 'like', '%'.$keyword.'%');
+                $query->where('r.name', 'like', '%'.$keyword.'%')
+                    ->orWhere('r.phone', 'like', '%'.$keyword.'%')
+                    ->orWhere('r.email', 'like', '%'.$keyword.'%')
+                    ->orWhere('r.company', 'like', '%'.$keyword.'%')
+                    ->orWhere('r.position', 'like', '%'.$keyword.'%')
+                    ->orWhere('r.title', 'like', '%'.$keyword.'%');
             })
-            ->get()->makeHidden(['is_delete','created_at','updated_at'])->toArray();
+
+            ->select('r.*','c.name as category_id_text')
+            ->orderBy('r.id','desc')
+            ->paginate($this->perPage);
+//            ->get()->makeHidden(['is_delete','created_at','updated_at'])->toArray();
 
     }
 
