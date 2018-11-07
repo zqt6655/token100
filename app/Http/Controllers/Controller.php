a19b9256aaf1c233c93ca533df40057a56425046
zqt6655 <125550581@qq.com>
2018-11-07 11:08:33 +0800

    校验token

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Validator;
 use Mrgoon\AliSms\AliSms;
 
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
+header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with,Origin,token');//自定义其他的头
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
