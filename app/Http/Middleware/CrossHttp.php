56a6e216df8ef91ed4f7dd3756be670d68fc9a75
zqt6655 <125550581@qq.com>
2018-11-08 10:30:12 +0800

    添加header中间件

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -16,7 +16,7 @@ class CrossHttp
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
+//        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Headers', 'token');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
